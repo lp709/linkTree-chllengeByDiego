c08ad15bfd4b36a13b37e5e5880a7cb5de44d8ba
Lp <pinheiroleandro7009@gmail.com>
2026-05-14 18:43:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const name = window.prompt('Digite seu nome:');
+window.alert(`Olá, ${name}! Seja bem-vindo(a) ao meu perfil!`);
+console.log('window.alert funcionou?');
+console.log(true);
+console.log('window.alert não funcionou?');
+console.error(false);
+const lpName = 'Leandro';
+const lpSurname = 'Jesus';
+const lpFullName = `${lpName} ${lpSurname}`;
+console.log('Name is : ' + lpFullName);
+console.log(lpFullName == lpFullName);
